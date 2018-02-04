eb0007736d40833d287a2fcf653e05661f101d8a
Guillaume Collin <guillaumecollin@MacBook-Pro-de-Guillaume.local>
2018-02-04 22:01:48 +0100

    Add saving options

--- code/DQL.py
+++ code/DQL.py
@@ -3,6 +3,7 @@ from keras.layers import Dense,Conv2D,Activation, Flatten
 from keras.optimizers import Adam
 import numpy as np
 import random
+from keras.models import load_model
 
 
 # Deep Q-learning Agent
@@ -12,21 +13,22 @@ class DQLAgent:
         self.image_height = 84
         self.image_width = 84
         self.aggregated_images = 4
-        self.memory=[]
-        self.learning_rate=1e-6
+        self.memory = []
+        self.learning_rate = 0.001
         self.action_size = action_size
         self.state_size = state_size
         self.epsilon = 1
-        self.epsilon_decay = 0.0001
+        self.epsilon_decay = 0.995
         self.epsilon_min = 0.1
-        self.gamma = 0.1
-        self.model=self._build_model()
+        self.gamma = 0.95
+        self.model = self._build_model()
+        self.max_size_memory = 100000
 
     def _build_model(self):
         model = Sequential()
-        model.add(Conv2D(16,(8,8),strides=(4,4),input_shape=(self.image_height, self.image_width, self.aggregated_images)))
+        model.add(Conv2D(16, (8,8), strides=(4,4), input_shape=(self.image_height, self.image_width, self.aggregated_images)))
         model.add(Activation('relu'))
-        model.add(Conv2D(32,(4,4),strides=(2,2)))
+        model.add(Conv2D(32,(4,4), strides=(2,2)))
         model.add(Activation('relu'))
         model.add(Dense(256))
         model.add(Activation('relu'))
@@ -35,8 +37,15 @@ class DQLAgent:
         model.compile(loss='mean_squared_error', optimizer=Adam(lr=self.learning_rate))
         return model
 
-    def add_to_memory(self, state, action, reward, next_state,terminal):
-        self.memory.append((state, action, reward, next_state,terminal))
+    def continue_model(self,file):
+        path = 'output/' + file + '.h5'
+        model = load_model(path)
+        self.model = model
+
+    def add_to_memory(self, state, action, reward, next_state, terminal):
+        self.memory.append((state, action, reward, next_state, terminal))
+        if len(self.memory) > self.max_size_memory:
+            self.memory.remove(self.memory[0])
 
     def act(self, state):
         #Greedy policy
@@ -44,19 +53,20 @@ class DQLAgent:
             return random.randrange(self.action_size)
         next_action = self.model.predict(state)
         if self.epsilon > self.epsilon_min:
-            self.epsilon -= self.epsilon_decay
+            self.epsilon *= self.epsilon_decay
         return np.argmax(next_action[0])  # returns action
 
     def replay(self, batch_size):
         minibatch = random.sample(self.memory,batch_size)
-        for state,action,reward,next_state,terminal in minibatch:
+        for state,action, reward,next_state,terminal in minibatch:
             if terminal:
                 y = reward
             else :
                 y = reward + self.gamma * np.max(self.model.predict(np.array(next_state)))
-            target = self.model.predict(state)
-            target[0][action] = y
-            self.model.fit(state, target, epochs=1, verbose=0)
+            y_cible = self.model.predict(state)
+            y_cible[0][action] = y
+            self.model.fit(state, y_cible, epochs=1, verbose=0)
 
-    def save(self,path):
+
+    def save(self, path):
         self.model.save(path)
--- code/main.py
+++ code/main.py
@@ -1,24 +1,42 @@
-#Utilisation de OpenAI Gym pour avoir l'agent
+# Utilisation de OpenAI Gym pour avoir l'agent
 
 import gym
 import time
 from DQL import DQLAgent
 from preprocess import preprocess
 from keras.models import load_model
+import pandas as pd
+import numpy as np
 
 env = gym.make('Breakout-v0')
 state_size = env.observation_space.shape
 action_size = env.action_space.n
-agent = DQLAgent(state_size,action_size)
-env.reset()
+agent = DQLAgent(state_size, action_size)
 
-
+######## A changer
+file = 'third_model'
 
 if __name__ == '__main__':
-    nb_episode = 10000
+    nb_episode = 1000000
     nb_step = 1000
     batch_size = 32
-    history = [0,0,0,0]
+    history = [0, 0, 0, 0]
+    save_models_each = 10000
+    restart = True
+    no_training_steps = 1000
+    allow_replay = False
+    load_model_from_file = 'second_model'
+    average_score = 0
+    list_average_score = []
+
+    if not restart:
+        # On charge le dernier modèle
+        agent.continue_model(load_model_from_file)
+        # Et les résultats
+        save_average_score = pd.read_csv('output/' + file + '.csv')
+    else:
+        save_average_score = pd.DataFrame({'Episode': [0],
+                                           'Average_score': [0]})
 
 
     for episode in range(nb_episode):
@@ -29,34 +47,52 @@ if __name__ == '__main__':
         history[2] = state
         history[3] = state
         compteur = 0
+        score = 0
         prepross_history = preprocess(history)
         for step in range(nb_step):
             compteur += 1
-            #Uncomment to unable rendering
-            #env.render()
+            # Uncomment to unable rendering
+            # env.render()
             # slepp is needed to correct an issue with pyglet/ mac os only ?
-            #time.sleep(0.1)
+            # time.sleep(0.1)
 
-            #on cherche la meilleure action
+            # on cherche la meilleure action
 
             action = agent.act(prepross_history)
 
             save_state = prepross_history
 
-            #On la fait
+            # On la fait
             next_state_not_processed, reward, done, info = env.step(action)
+            score += reward
 
             history[0:3] = history[1:4]
             history[3] = next_state_not_processed
 
-            next_state=preprocess(history)
-            prepross_history=next_state
-            #On la sauvegarde
-            agent.add_to_memory(state=save_state,action=action,reward=reward,next_state=next_state,terminal=done)
+            next_state = preprocess(history)
+            prepross_history = next_state
+            # On la sauvegarde
+            agent.add_to_memory(state=save_state, action=action, reward=reward, next_state=next_state, terminal=done)
 
             if done:
-                print('Agent has reached the end in ' + str(compteur) + ' steps')
+                message = 'Agent has reached the end in {} steps with score {}'.format(compteur, score)
+                print(message)
                 break
             state = next_state
-        agent.replay(batch_size)
-    agent.save('output/first_model.h5')
+
+            if allow_replay:
+                agent.replay(batch_size)
+        average_score += score
+        if episode % save_models_each == 0:
+            agent.save('output/backup.h5')
+            list_average_score.append((nb_episode, average_score / 1000))
+            average_score = 0
+            print('Le score moyen sur les 1000 derniers épisode était {}'.format(average_score))
+        if no_training_steps > 0:
+            no_training_steps -= compteur
+        else:
+            allow_replay = True
+    save_average_score[0] = np.array(list_average_score)[:, 0]
+    save_average_score[1] = np.array(list_average_score)[:, 1]
+    save_average_score.to_csv('output/' + file + '.csv')
+    agent.save('output/' + file + '.h5')
