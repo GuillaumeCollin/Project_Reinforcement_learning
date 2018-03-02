399a149d770961ec4e13d3c5f9391681fec0d9d9
Guillaume Collin <guillaumecollin@macbook-pro-de-guillaume.home>
2018-03-02 15:52:33 +0100

    Fix

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 def to_gray(image):
     gray_image =  np.dot(image[...,:3], [0.299, 0.587, 0.114])
@@ -9,5 +11,6 @@ def preprocess(images):
     for i in range(4):
         corp_image[0:210,0:160,i] = to_gray(images[i])
     ressampled_image = corp_image[::2,::2]
-    return  np.expand_dims(ressampled_image[13:97], axis=0)
+    img = np.expand_dims(ressampled_image[13:97], axis=0).astype(np.uint8)
+    return img
 
